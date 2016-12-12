339db879810c4e92cb6634eac9ef2a82ede4ad2a
Albert Street <alstreet@mini.home>
2016-12-12 08:26:25 -0500

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -13,7 +13,11 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples Component!</h3>
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to the Examples page!</p>
+    </div>
+
   );
 };
 
