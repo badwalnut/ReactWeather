03295b9e2a2a3c62c47e13a65bff966338fe3fdf
Albert Street <alstreet@mini.home>
2016-12-14 07:02:51 -0500

    Added some styling with Foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var Examples = React.createClass({
 //   render: function () {
 //     return (
@@ -14,11 +14,21 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component!</h3>
-      <p>Welcome to the Examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
 
-  );
+  )
 };
 
 module.exports = Examples;
+
+// rem
