806e31de2223d0eefef64651c5bff96c245973cf
Albert Street <alstreet@mini.home>
2016-12-15 07:08:52 -0500

    Styled and added links to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -18,6 +18,16 @@ var About = (props) => {
       <p>React Weather</p>
       <p>An app created as a tool for learning React.js</p>
       <p>Gets current temperature for queried city</p>
+      <p></p>
+      <p>Some of the tools used were:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Open Weather Map was used to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
